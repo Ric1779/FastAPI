27ca9b4f9084d5b2fdd390b4da974fba6a82658e
Richards <richards.1779@gmail.com>
2026-06-04 21:05:17 +0200

    Jinja2 Templates

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+# first argument is the url path where the static files will be accessible
+# the second one is an instance of Staticfiles pointing to the static directory
+# name that is used to reference in templates
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
 
 posts: list[dict] = [
     {
@@ -21,11 +28,13 @@ posts: list[dict] = [
     },
 ]
 
-
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
-def home():
-    return f"<h1>{posts[0]['title']}</h1>"
+# `request` represents the incoming HTTP request (headers, URL, client info, etc.)
+# It is required by TemplateResponse to provide context to the HTML template
+# (e.g., URL generation, accessing request data inside the template)
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html", {"posts": posts, "title": "Home"})
 
 @app.get("/api/posts")
 def get_posts():
